cc9edea560e74b16cd47092effecf1e539ab8f94
Hendry <hendry@icehousecorp.com>
2015-11-11 10:20:31 +0700

    create databaseConnector object

--- src/service/databaseconnector.js
+++ src/service/databaseconnector.js
@@ -1,4 +1,6 @@
 var pg = require('pg');
 var conString = "postgres://gxcwajlqnngmcd:Zwn-y92PuvU3_POXPm4IXjMq-x@ec2-54-204-6-113.compute-1.amazonaws.com:5432/d4kfkhe3k34bbp"
 
+var databaseConnector = new Object();
+
 module.exports = databaseConnector;
