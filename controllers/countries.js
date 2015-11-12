94935d991fe7c678fef28658a7187146fbe00733
Hendry <hendry@icehousecorp.com>
2015-11-12 14:04:52 +0700

    Add single country get endpoint

--- controllers/countries.js
+++ controllers/countries.js
@@ -38,4 +38,33 @@ router.post('/', function(req, res) {
   });
 });
 
+router.use('/:id', function(req, res, next) {
+  var id = Number(req.params.id);
+
+  if(_.isNumber(id)) {
+    next();
+  } else {
+    res.status(400).send("Value should be a number");
+  }
+});
+
+router.get('/:id', function(req, res) {
+  var query = "SELECT * FROM countries WHERE country_id = $1";
+  var values = [req.params.id];
+
+  db.query(query, values)
+  .then(function(result) {
+    var row = result.rows[0];
+
+    if(_.isEmpty(row)) {
+      res.status(404).end();
+    } else {
+      res.status(200).send(result.rows[0]);
+    }
+  })
+  .catch(function(error) {
+    res.status(500).send(error);
+  });
+});
+
 module.exports = router;
