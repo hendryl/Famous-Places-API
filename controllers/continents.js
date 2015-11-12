314f017dfb57e2bf71a68d63f91680aae039e019
Hendry <hendry@icehousecorp.com>
2015-11-12 11:57:49 +0700

    Fix query error

--- controllers/continents.js
+++ controllers/continents.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  var query = "SELECT * FROM continent";
+  var query = "SELECT * FROM continents";
 
   db.query(query)
   .then(function(result) {
