c64ff9fa99e9674bd0ac27316cc82abcaa13791f
Hendry <hendry@icehousecorp.com>
2015-11-12 10:55:36 +0700

    Add single id getter

--- controllers/continents.js
+++ controllers/continents.js
@@ -1,4 +1,5 @@
 var db = require('../helpers/db');
+var _ = require('underscore');
 
 var express = require('express');
 var router = express.Router();
@@ -8,25 +9,53 @@ router.get('/', function(req, res) {
 
   db.query(query)
   .then(function(result) {
-    res.status(200).json(result.rows);
+    res.status(200).send(result.rows);
   })
   .catch(function(error) {
-    res.status(400).send(error);
+    res.status(500).send(error);
   });
 });
 
 router.post('/', function(req, res) {
-  var continent = req.body.continent;
+  var name = req.body.name;
+
+  if(!(_.isString(name))) {
+    res.status(400).send("Bad Request: no name parameter");
+    return;
+  }
+
   var query = 'INSERT INTO continent ("name") VALUES ($1) RETURNING continent_id';
-  var values = [continent];
+  var values = [name];
 
   db.query(query, values)
   .then(function(result) {
     res.status(201).send(result.rows[0]);
   })
   .catch(function(error) {
-    console.log(error);
-    res.status(400).send(error);
+    res.status(500).send(error);
+  });
+});
+
+router.use('/:id', function(req, res, next) {
+  var id = Number(req.params.id);
+
+  if(_.isNumber(id)) {
+    next();
+  } else {
+    res.status(400).send("Value should be a number");
+  }
+});
+
+router.get('/:id', function(req, res) {
+  var query = "SELECT * FROM continent WHERE continent_id = $1";
+  var values = [req.params.id];
+
+  db.query(query, values)
+  .then(function(result) {
+    res.status(200).send(result.rows[0]);
+  })
+  .catch(function(error) {
+    res.status(500).send(error);
   });
 });
 
