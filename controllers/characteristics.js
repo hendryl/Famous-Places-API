706d119fbaba3e12e46e3fcf763c237c95d5ef68
Hendry <hendry@icehousecorp.com>
2015-11-12 15:33:23 +0700

    Create GET for characteristics endpoint

---
+++ controllers/characteristics.js
@@ -0,0 +1,19 @@
+var db = require('../helpers/db');
+var _ = require('underscore');
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  var query = "SELECT * FROM characteristics";
+
+  db.query(query)
+  .then(function(result) {
+    res.status(200).send(result.rows);
+  })
+  .catch(function(error) {
+    res.status(500).send(error);
+  });
+});
+
+module.exports = router;
