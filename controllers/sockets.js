ac1971840d4a0f80192a191196ac0802a75334fa
Hendry <email.hendry.lee@gmail.com>
2015-12-25 17:50:47 +0700

    Create basic socket connection

---
+++ controllers/sockets.js
@@ -0,0 +1,11 @@
+var prepare = function(io) {
+
+  io.on('connection', function(socket) {
+    console.log('User ' + socket.id + ' connected');
+    socket.on('disconnect', function() {
+      console.log('User ' + socket.id + ' disconnected');
+    });
+  });
+};
+
+module.exports = prepare;
